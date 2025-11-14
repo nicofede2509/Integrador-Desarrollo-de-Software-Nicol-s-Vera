059d3f936998b3d79e38f630a7ef51814b4c8868
nicofede2509 <nicolasfedericovy12@gmail.com>
2025-11-14 17:57:23 -0300

    Metodos en endpoints, cálculo de hash, clase StatsResponse completa y clase StatsService completa

--- src/main/java/com/example/examenmercado/service/StatsService.java
+++ src/main/java/com/example/examenmercado/service/StatsService.java
@@ -2,20 +2,18 @@ package com.example.examenmercado.service;
 
 import com.example.examenmercado.dto.StatsResponse;
 import com.example.examenmercado.repository.DnaRecordRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class StatsService {
     private final DnaRecordRepository repositorio;
-
-    public StatsService(DnaRecordRepository repositorio) {
-        this.repositorio = repositorio;
-    }
 
     public StatsResponse getRatio(){
         long contadorMutantes = repositorio.countByIsMutant(true);
         long contadorHumanos = repositorio.countByIsMutant(false);
         double ratio = (contadorHumanos == 0) ? contadorMutantes : (double) contadorMutantes/contadorHumanos;
-        return new StatsResponse(contadorMutantes, contadorHumanos, ratio);
+        return new StatsResponse(contadorHumanos, contadorMutantes, ratio);
     }
 }
